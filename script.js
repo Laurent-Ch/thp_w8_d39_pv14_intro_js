c212da4851121d2a2a16d21076532d4456a5e936
Laurent-Ch <chamouleau.laurent@gmail.com>
2021-11-18 12:29:38 +0100

    startup first question

--- script.js
+++ script.js
@@ -32,3 +32,37 @@ exo4Btn.addEventListener('click', e => {
     console.log(space + block);
   }
 })
+
+const entrepreneurs = [
+  { first: 'Steve', last: 'Jobs', year: 1955 },
+  { first: 'Oprah', last: 'Winfrey', year: 1954 },
+  { first: 'Bill', last: 'Gates', year: 1955 },
+  { first: 'Sheryl', last: 'Sandberg', year: 1969 },
+  { first: 'Mark', last: 'Zuckerberg', year: 1984 },
+  { first: 'Beyonce', last: 'Knowles', year: 1981 },
+  { first: 'Jeff', last: 'Bezos', year: 1964 },
+  { first: 'Diane', last: 'Hendricks', year: 1947 },
+  { first: 'Elon', last: 'Musk', year: 1971 },
+  { first: 'Marissa', last: 'Mayer', year: 1975 },
+  { first: 'Walt', last: 'Disney', year: 1901 },
+  { first: 'Larry', last: 'Page', year: 1973 },
+  { first: 'Jack', last: 'Dorsey', year: 1976 },
+  { first: 'Evan', last: 'Spiegel', year: 1990 },
+  { first: 'Brian', last: 'Chesky', year: 1981 },
+  { first: 'Travis', last: 'Kalanick', year: 1976 },
+  { first: 'Marc', last: 'Andreessen', year: 1971 },
+  { first: 'Peter', last: 'Thiel', year: 1967 }
+];
+
+// Exercice 5
+let exo5_1Btn = document.querySelector('.script-5-1');
+exo5_1Btn.addEventListener('click', e => {
+  entrepreneurs.forEach(entrepeneur => {
+    // console.log(`test`);
+    if (entrepeneur.year >= 1970 && entrepeneur.year < 1980) {
+      console.log(`${entrepeneur.first} ${entrepeneur.last}, ${entrepeneur.year}`);
+    }
+  }
+  )
+}
+)
